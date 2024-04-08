f471af30b9f60d6e630a4504f51991392f5147b0
inzamam <inzamam.pawaskar10@gmail.com>
2024-04-08 22:50:46 +0500

    feat: Integarted BrightData for scraping, Created functions to convert scrapped data in consumable format and Integrated mongodb database for string data

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,65 @@
-"use client"
-import React from 'react'
+"use client";
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import React, { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+
+    //Check is hostname contains amazon.com or amazon.in
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+
+    return false
+  } catch (error) {
+    return false
+  }
+};
 
 const Searchbar = () => {
-    const handleSubmit = ()=>{}
-  return (
-    <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter product link' className='searchbar-input' />
-        <button type='submit' className='searchbar-btn'>
-            Search
-        </button>
-    </form>
-  )
-}
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Searchbar
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if (!isValidLink) return alert("Please provide a valid amazon link");
+
+    try {
+      setIsLoading(true);
+
+      //Scrape product
+      const product = await scrapeAndStoreProduct(searchPrompt)
+    } catch (error) {
+      console.log(error);
+    } finally{
+      setIsLoading(false)
+    }
+  };
+
+
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button type="submit" className="searchbar-btn" disabled={searchPrompt === ""}>
+        {isLoading ? "Searching..." : "Search"}
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;
